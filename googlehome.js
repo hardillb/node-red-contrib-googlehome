a22676c9c9981c9b997b357feafd0b9315a835f4
Ben Hardill <hardillb@gmail.com>
2019-03-04 15:38:03 +0000

    Add out of band responses

--- googlehome.js
+++ googlehome.js
@@ -19,26 +19,26 @@ var devicesURL = process.env.GOOGLEHOME_URL || 'https://googlehome.hardill.me.uk
 
 module.exports = function(RED) {
 
-	"use strict";
-	const fs = require('fs');
-	const mqtt = require('mqtt');
-	const path = require('path');
-	const request = require('request');
+  "use strict";
+  const fs = require('fs');
+  const mqtt = require('mqtt');
+  const path = require('path');
+  const request = require('request');
 
-	var devices = {};
+  var devices = {};
 
-	var caPath = path.join(__dirname, "ca-chain.pem");
+  var caPath = path.join(__dirname, "ca-chain.pem");
 
-	function googleHomeConf(n) {
-		RED.nodes.createNode(this,n);
-		this.username = n.username;
-		this.password = this.credentials.password;
+  function googleHomeConf(n) {
+    RED.nodes.createNode(this,n);
+    this.username = n.username;
+    this.password = this.credentials.password;
 
-		this.users = {};
+    this.users = {};
 
-		var node = this;
+    var node = this;
 
-		var options = {
+    var options = {
       username: node.username,
       password: node.password,
       clientId: node.username,
@@ -58,32 +58,32 @@ module.exports = function(RED) {
       ]
     };
 
-		getDevices(this.username, this.password, node.id);
+    getDevices(this.username, this.password, node.id);
 
-		this.connect = function() {
-			node.client = mqtt.connect(options);
-			node.client.setMaxListeners(0);
+    this.connect = function() {
+      node.client = mqtt.connect(options);
+      node.client.setMaxListeners(0);
 
-			node.client.on('connect', function() {
-					node.setStatus({text:'connected', shape:'dot', fill:'green'});
-					node.client.removeAllListeners('message');
-					node.client.subscribe("command/" + node.username + "/#");
-					node.client.on('message', function(topic, message) {
-						//tricky bit
-						var msg = JSON.parse(message.toString());
-						var devId = msg.id;
-						for (var id in node.users) {
-							if (node.users.hasOwnProperty(id)){
-								if (node.users[id].device === devId) {
-									node.users[id].command(msg);
-								}
-							}
-						}
-					});
-				}
-			);
+      node.client.on('connect', function() {
+          node.setStatus({text:'connected', shape:'dot', fill:'green'});
+          node.client.removeAllListeners('message');
+          node.client.subscribe("command/" + node.username + "/#");
+          node.client.on('message', function(topic, message) {
+            //tricky bit
+            var msg = JSON.parse(message.toString());
+            var devId = msg.id;
+            for (var id in node.users) {
+              if (node.users.hasOwnProperty(id)){
+                if (node.users[id].device === devId) {
+                  node.users[id].command(msg);
+                }
+              }
+            }
+          });
+        }
+      );
 
-			node.client.on('offline',function() {
+      node.client.on('offline',function() {
         node.setStatus({text: 'disconnected', shape: 'dot', fill:'red'});
       });
 
@@ -96,163 +96,189 @@ module.exports = function(RED) {
         node.setStatus({text: 'disconnected', shape: 'dot', fill:'red'});
         node.error(err);
       });
-		};
+    };
 
-		this.setStatus = function(status) {
-			for( var id in node.users) {
-      	if (node.users.hasOwnProperty(id)) {
+    this.setStatus = function(status) {
+      for( var id in node.users) {
+        if (node.users.hasOwnProperty(id)) {
           node.users[id].status(status);
         }
       }
-		}
+    }
 
-		this.register = function(deviceNode) {
-			node.users[deviceNode.id] = deviceNode;
-			if (Object.keys(node.users).length === 1) {
-				node.connect();
-			} else {
-				node.setStatus({text:'connected', shape:'dot', fill:'green'});
-			}
-		}
+    this.register = function(deviceNode) {
+      node.users[deviceNode.id] = deviceNode;
+      if (Object.keys(node.users).length === 1) {
+        node.connect();
+      } else {
+        node.setStatus({text:'connected', shape:'dot', fill:'green'});
+      }
+    }
 
-		this.unregister = function(deviceNode, done) {
-			delete node.users[deviceNode.id];
+    this.unregister = function(deviceNode, done) {
+      delete node.users[deviceNode.id];
 
-			if (Object.keys(node.users).length === 0) {
-				if (node.client && node.client.connected) {
-        	node.client.end(done);
+      if (Object.keys(node.users).length === 0) {
+        if (node.client && node.client.connected) {
+          node.client.end(done);
         } else {
           node.client.end();
           done();
         }
-			}
+      }
 
-			done();
-		}
+      done();
+    }
 
-		this.acknowledge = function(msg) {
-			var topic = "response/" + node.username;
-			node.client.publish(topic, JSON.stringify(msg));
-		}
+    this.acknowledge = function(msg) {
+      var topic = "response/" + node.username;
+      node.client.publish(topic, JSON.stringify(msg));
+    }
 
-		this.on('close', function(){
-			if (node.client && node.client.connected) {
+    this.reportState = function(msg) {
+      var topic = "status/" + node.username;
+      node.client.publish(topic, JSON.stringify(msg));
+    }
+
+    this.on('close', function(){
+      if (node.client && node.client.connected) {
         node.client.end();
       }
-		});
-	}
+    });
+  }
 
-	RED.nodes.registerType("google-home-conf",googleHomeConf,{
-  	credentials: {
-    	password: {type:"password"}
+  RED.nodes.registerType("google-home-conf",googleHomeConf,{
+    credentials: {
+      password: {type:"password"}
     }
   });
 
   function googleHome(n) {
-  	RED.nodes.createNode(this,n);
-  	this.conf = RED.nodes.getNode(n.conf);
-  	this.confId = n.conf;
-  	this.acknowledge = n.acknowledge;
-  	this.topic = n.topic;
-  	this.device = n.device;
-  	this.name = n.name;
+    RED.nodes.createNode(this,n);
+    this.conf = RED.nodes.getNode(n.conf);
+    this.confId = n.conf;
+    this.acknowledge = n.acknowledge;
+    this.topic = n.topic;
+    this.device = n.device;
+    this.name = n.name;
 
-  	var node = this;
+    var node = this;
 
-  	node.command = function(msg) {
-  		var m = {
-  			topic: node.topic || "",
+    node.command = function(msg) {
+      var m = {
+        topic: node.topic || "",
         name: node.name,
+        deviceId: msg.id,
+        _requestId: msg.requestId,
+        _raw: msg,
         _confId: node.confId,
-        payload: msg
-  		}
-  		node.send(m);
-  		if (node.acknowledge) {
-  			msg.status = true;
-  			node.conf.acknowledge(msg);
-  		}
-  	}
+        payload: msg.execution
+      }
+      node.send(m);
+      if (node.acknowledge) {
+        msg.status = true;
+        node.conf.acknowledge(msg);
+      }
+    }
 
-  	node.conf.register(node);
+    node.conf.register(node);
 
     node.on('close', function(done){
-    	node.conf.unregister(node, done);
+      node.conf.unregister(node, done);
     });
   }
 
   RED.nodes.registerType("google-home", googleHome);
 
   function googleHomeResponse(n) {
-  	RED.nodes.createNode(this,n);
-  	this.conf = RED.nodes.getNode(n.conf);
-  	this.confId = n.conf;
+    RED.nodes.createNode(this,n);
+    this.conf = RED.nodes.getNode(n.conf);
+    this.confId = n.conf;
+    this.deviceId = n.device;
 
-  	var node = this;
+    var node = this;
 
-  	node.on('input', function(msg){
-  		if (node.confId == msg._confId) {
-  			if (msg.payload.requestId) {
-  				node.conf.acknowledge(msg);
-  			} else {
-  				//TODO report state
-  			}
-  		} else {
-  			//miss match
-  			node.error("Input message confId does not match configuration")
-  		}
+    node.on('input', function(msg){
+      //console.log(msg)
+      if (msg._confId) {
+        if (msg._confId == node.confId) {
+          if (msg._requestId) {
+            //console.log("replying to a command")
+            var resp = {
+              requestId: msg._requestId,
+              id: msg.deviceId,
+              execution: msg.payload
+            }
+            node.conf.acknowledge(resp);
+          } else {
+            // no request id so something wrong
+            node.error("No _confId but no _requestId",msg);
+          }
+        } else {
+          node.error("Input message confId does not match configuration", msg);
+        }
+      } else {
+        // no conf id in message so must be a report
+        // console.log("status change");
+        var resp = {
+          id: node.deviceId,
+          execution: msg.payload
+        }
+        node.conf.reportState(resp);
+      }
 
-  	})
+    })
   }
 
   RED.nodes.registerType("google-home-response", googleHomeResponse);
 
-	function getDevices(username, password, id) {
-		if (username && password) {
-			request.get({
-				url: devicesURL,
-				auth: {
-					username: username,
-					password: password
-				}
-			}, function(err,res,body) {
-				if (!err && res.statusCode == 200) {
-					var devs = JSON.parse(body);
-					devices[id] = devs
-				} else {
-					//console.("err: " + err);
+  function getDevices(username, password, id) {
+    if (username && password) {
+      request.get({
+        url: devicesURL,
+        auth: {
+          username: username,
+          password: password
+        }
+      }, function(err,res,body) {
+        if (!err && res.statusCode == 200) {
+          var devs = JSON.parse(body);
+          devices[id] = devs
+        } else {
+          //console.("err: " + err);
           RED.log.log("Problem looking up " + username + "'s devices");
-				}
-			});
-		}
-	}
+        }
+      });
+    }
+  }
 
-	RED.httpAdmin.post('/google-home/new-account',function(req,res){
-   	var username = req.body.user;
-   	var password = req.body.pass;
-   	var id = req.body.id;
-  	getDevices(username,password,id);
+  RED.httpAdmin.post('/google-home/new-account',function(req,res){
+    var username = req.body.user;
+    var password = req.body.pass;
+    var id = req.body.id;
+    getDevices(username,password,id);
   });
 
-	RED.httpAdmin.post('/google-home/refresh/:id', function(req,res){
-		var id = req.params.id;
-		var conf = RED.nodes.getNode(id);
-		if (conf) {
-			var username = conf.username;
-			var password = conf.credentials.password;
-			getDevices(username,password,id);
-			res.status(200).send();
-		} else {
-			//not deployed yet
-			console.log("Can't refresh until deployed");
-			res.status(404).send();
-		}
+  RED.httpAdmin.post('/google-home/refresh/:id', function(req,res){
+    var id = req.params.id;
+    var conf = RED.nodes.getNode(id);
+    if (conf) {
+      var username = conf.username;
+      var password = conf.credentials.password;
+      getDevices(username,password,id);
+      res.status(200).send();
+    } else {
+      //not deployed yet
+      console.log("Can't refresh until deployed");
+      res.status(404).send();
+    }
   });
 
   RED.httpAdmin.get('/google-home/devices/:id', function(req,res){
-  	if (devices[req.params.id]) {
-    	res.send(devices[req.params.id]);
+    if (devices[req.params.id]) {
+      res.send(devices[req.params.id]);
     } else {
-    	res.status(404).send();
+      res.status(404).send();
     }
   });
 }
